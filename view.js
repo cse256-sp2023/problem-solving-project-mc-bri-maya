959b3d81563ce487d2e767eed8dd853ccedd30db
mcgatter <59850149+mcgatter@users.noreply.github.com>
2023-04-14 16:47:56 -0500

    button description change

--- view.js
+++ view.js
@@ -133,7 +133,7 @@ function make_file_element(file_obj) {
             <h3 id="${file_hash}_header">
                 <span class="oi oi-folder" id="${file_hash}_icon"/> ${file_obj.filename} 
                 <button class="ui-button ui-widget ui-corner-all permbutton" path="${file_hash}" id="${file_hash}_permbutton"> 
-                    <span class="oi oi-lock-unlocked" id="${file_hash}_permicon"/> 
+                    Change Folder Permissions <span class="oi oi-lock-unlocked" id="${file_hash}_permicon"/> 
                 </button>
             </h3>
         </div>`)
@@ -153,7 +153,7 @@ function make_file_element(file_obj) {
         return $(`<div class='file'  id="${file_hash}_div">
             <span class="oi oi-file" id="${file_hash}_icon"/> ${file_obj.filename}
             <button class="ui-button ui-widget ui-corner-all permbutton" path="${file_hash}" id="${file_hash}_permbutton"> 
-                <span class="oi oi-lock-unlocked" id="${file_hash}_permicon"/> 
+                Change File Permissions <span class="oi oi-lock-unlocked" id="${file_hash}_permicon"/> 
             </button>
         </div>`)
     }
